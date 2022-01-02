4c7f38f746fb13a9f56c5e0ee099ac30b9b2edb7
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2022-01-02 15:33:30 +0000

    basketAdded

---
+++ API/DTOs/BasketItemDto.cs
@@ -0,0 +1,20 @@
+namespace API.DTOs
+{
+    public class BasketItemDto
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public long Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Type { get; set; }
+
+    }
+}
