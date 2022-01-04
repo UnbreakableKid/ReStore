689b6f70f4822f99933bc0dfd3ecf3142ec8fcf2
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2022-01-04 15:51:08 +0000

    setup contrroller for filtering & searching

---
+++ API/Extensions/ProductExtensions.cs
@@ -0,0 +1,56 @@
+using API.Entities;
+
+namespace API.Extensions
+{
+    public static class ProductExtensions
+    {
+        public static IQueryable<Product> Sort(this IQueryable<Product> query, string? orderBy)
+        {
+            query = orderBy switch
+            {
+                "name" => query.OrderBy(p => p.Name),
+                "price" => query.OrderBy(p => p.Price),
+                "priceDesc" => query.OrderByDescending(p => p.Price),
+                _ => query.OrderBy(p => p.Name)
+
+            };
+
+            return query;
+        }
+
+        public static IQueryable<Product> Search(this IQueryable<Product> query, string? searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                return query;
+            }
+            var lowerCased = searchTerm.Trim().ToLowerInvariant();
+
+            return query.Where(p => p.Name.ToLower().Contains(lowerCased));
+        }
+
+        public static IQueryable<Product> Filter(this IQueryable<Product> query, string? brands, string? types)
+        {
+            var brandList = new List<string>();
+            var typeList = new List<string>();
+
+            if (!string.IsNullOrEmpty(brands))
+            {
+                brandList.AddRange(brands.ToLower().Split(',').ToList());
+            }
+
+            if (!string.IsNullOrEmpty(types))
+            {
+                typeList.AddRange(types.ToLower().Split(',').ToList());
+            }
+
+            query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Brand.ToLower())).
+                Where(p => typeList.Count == 0 || typeList.Contains(p.Type.ToLower()));
+
+            return query;
+
+        }
+
+
+    }
+}
